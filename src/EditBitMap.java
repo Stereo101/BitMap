246f5e626fb41cb02fe03d3a79012bfcb83c654e
Stereo101 <Stereo_101@mac.com>
2015-11-03 13:38:08 -0800

    Added frame work of (Selector/Demon), undo/redo, and editing image loading sceme

--- src/EditBitMap.java
+++ src/EditBitMap.java
@@ -1,7 +1,9 @@
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -12,22 +14,15 @@ import java.lang.Byte;
 
 public class EditBitMap {
 	public static void main(String[] args) throws IOException {
-		File file = new File("temp.txt");
-		Path path = Paths.get(file.getAbsolutePath());
 		
-		byte[] image = null;
+		BitMap lain = new BitMap("input",1);
+		Selector choice = new Selector(lain);
+		Demon belial = new Demon(choice);
+		choice.all(1,"-r");
+		choice.all(2);
+		belial.swap(1,2);
 		
-		try {
-			image = Files.readAllBytes(path.resolveSibling("input.bmp"));
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		BitMap lain = new BitMap(image,1);
-		lain.reloaderDemon(1,2);
 		lain.genNewBitMap("output");
-		
 	}
 }
 
@@ -54,7 +49,11 @@ class BitMap {
 	
 	long SizeColorTable;
 	byte[] Header;
-	byte[][] pixelArray;
+	byte[][] pixels;
+	
+	byte[][][] pixelsHistory = new byte[100][][];
+	int histCount = 0;;
+	int redos = 0;
 	
 	
 	
@@ -64,7 +63,20 @@ class BitMap {
 	//
 	//Slide and Div are 2 modifiers that change how the pixelArray is loaded
 	//Using slide = div = 1 will load in correctly
-	public BitMap(byte[] data, double slide) {
+	public BitMap(String name, double slide) {
+		File file = new File("temp.txt");
+		Path path = Paths.get(file.getAbsolutePath());
+		
+		byte[] data = null;
+		
+		try {
+			data = Files.readAllBytes(path.resolveSibling(name + ".bmp"));
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
 		System.out.print("\nSignature: ");
 		longFromRange(1,2,data);
 		System.out.print("FileSize: ");
@@ -132,15 +144,17 @@ class BitMap {
 		}
 		
 		
-		pixelArray = new byte[(int)((FileSize - DataOffset)/(BitCount/8))][(int)BitCount/8];
+		pixels = new byte[(int)((FileSize - DataOffset)/(BitCount/8))][(int)BitCount/8];
 		for(int i = 0, t = 0; i<(int)((FileSize-DataOffset)/(BitCount/8)); i++) {
 			for(int q = 0; q<(int)(BitCount/8); q++,t++) {
-				pixelArray[i][q] = data[(int)((double)t*slide)%(int)(FileSize-DataOffset)];
+				pixels[i][q] = data[(int)((double)t*slide)%(int)(FileSize-DataOffset)];
 			}
 		}
 		
+		pixelsHistory[0] = deepCopyPixels(pixels);
+		
 		System.out.println("\n\n\n");
-		System.out.println(pixelArray.length);
+		System.out.println("There are "  + pixels.length + " pixels in this image");
 	}
 	
 	
@@ -154,7 +168,7 @@ class BitMap {
 		}
 		
 		for(int i = Header.length; i<(int) (FileSize - DataOffset - 1); i++) {
-			out[i] = pixelArray[i/(int)(BitCount/8)][i%(int)(BitCount/8)];
+			out[i] = pixels[i/(int)(BitCount/8)][i%(int)(BitCount/8)];
 		}
 		
 		FileOutputStream fos = new FileOutputStream(name + ".bmp");
@@ -166,14 +180,14 @@ class BitMap {
 	//For Each Pixel, Flip Coin: Heads:Swap with previous; Tails:Swap with next;
 	void fuzzByPixelDemon(int repeat) {
 		for(int rep = 0; rep < repeat; rep++) {
-			for(int i = 0; i< pixelArray.length;i++) {
+			for(int i = 0; i< pixels.length;i++) {
 				if(Math.random() > .5) {
 					if(i-1 > 0) {
-						swapByteArray(pixelArray[i],pixelArray[i-1]);
+						swapByteArray(pixels[i],pixels[i-1]);
 					}
 				} else {
-					if(i+1 < pixelArray.length) {
-						swapByteArray(pixelArray[i],pixelArray[i+1]);
+					if(i+1 < pixels.length) {
+						swapByteArray(pixels[i],pixels[i+1]);
 					}
 				}
 			}
@@ -192,13 +206,13 @@ class BitMap {
 					if(Math.random() > .5) {
 						if(i+1 < (int) width){
 							for(int q = 0; q < (int)Width; q++) {
-								swapByteArray(pixelArray[(int)(i*width + q)%(int)pixelArray.length],pixelArray[(int)((i+1)*width + q)%(int)pixelArray.length]);
+								swapByteArray(pixels[(int)(i*width + q)%(int)pixels.length],pixels[(int)((i+1)*width + q)%(int)pixels.length]);
 							}
 						}
 					} else {
 						if(i-1 > 0){
 							for(int q = 0; q < (int)Width - 1; q++) {
-								swapByteArray(pixelArray[((int)(i*width) + q)%(int)pixelArray.length],pixelArray[((int)((i-1)*width) + q)%(int)pixelArray.length]);
+								swapByteArray(pixels[((int)(i*width) + q)%(int)pixels.length],pixels[((int)((i-1)*width) + q)%(int)pixels.length]);
 							}
 						}
 					}
@@ -214,13 +228,13 @@ class BitMap {
 					if(Math.random() > .5) {
 						if(i+1 < (int) Width){
 							for(int q = 0; q < (int)Height; q++) {
-								swapByteArray(pixelArray[i + q*(int)Width],pixelArray[(i+1) + q*(int)Width]);
+								swapByteArray(pixels[i + q*(int)Width],pixels[(i+1) + q*(int)Width]);
 							}
 						}
 					} else {
 						if(i-1 > 0){
 							for(int q = 0; q < (int)Height; q++) {
-								swapByteArray(pixelArray[i + q*(int)Width],pixelArray[(i-1) + q*(int)Width]);
+								swapByteArray(pixels[i + q*(int)Width],pixels[(i-1) + q*(int)Width]);
 							}
 						}
 					}
@@ -236,13 +250,13 @@ class BitMap {
 				if(Math.random() > .5) {
 					if(i+1 < (int) Width){
 						for(int q = 0; q < (int)Height + 40; q++) {
-							swapByteArray(pixelArray[(i + q*(int)((float)Width*1))%pixelArray.length],pixelArray[((i+1) + q*(int)((float)Width*1.001925))%pixelArray.length]);
+							swapByteArray(pixels[(i + q*(int)((float)Width*1))%pixels.length],pixels[((i+1) + q*(int)((float)Width*1.001925))%pixels.length]);
 						}
 					}
 				} else {
 					if(i-1 > 0){
 						for(int q = 0; q < (int)Height; q++) {
-							swapByteArray(pixelArray[(i + q*(int)((float)Width*1))%pixelArray.length],pixelArray[((i-1) + q*(int)((float)Width*1))%pixelArray.length]);
+							swapByteArray(pixels[(i + q*(int)((float)Width*1))%pixels.length],pixels[((i-1) + q*(int)((float)Width*1))%pixels.length]);
 						}
 					}
 				}
@@ -253,14 +267,14 @@ class BitMap {
 	
 	void ColorDemon(int repeat, int shiftAmount, boolean doComplement, boolean randomSwap, int pushAmount,int colorByteToSwap1, int colorByteToSwap2) {
 		for(int rep = 0; rep < repeat; rep++) {
-			for(int i = 0; i<pixelArray.length; i++) {
+			for(int i = 0; i<pixels.length; i++) {
 				byte hold;
-				if(randomSwap){colorByteToSwap1 = (int)(Math.random() * pixelArray[0].length); colorByteToSwap2 = (int)(Math.random() * pixelArray[0].length);}
-				hold = pixelArray[i][colorByteToSwap1];
+				if(randomSwap){colorByteToSwap1 = (int)(Math.random() * pixels[0].length); colorByteToSwap2 = (int)(Math.random() * pixels[0].length);}
+				hold = pixels[i][colorByteToSwap1];
 				hold =  (byte)(hold << shiftAmount);
 				if(doComplement) {hold = (byte) (~hold);}
-				pixelArray[i][colorByteToSwap1] = pixelArray[i][colorByteToSwap2];
-				pixelArray[(i+pushAmount)%pixelArray.length][colorByteToSwap2] = hold;
+				pixels[i][colorByteToSwap1] = pixels[i][colorByteToSwap2];
+				pixels[(i+pushAmount)%pixels.length][colorByteToSwap2] = hold;
 			}
 		}
 	}
@@ -268,24 +282,24 @@ class BitMap {
 	//
 	void CarelessSwapDemon(int repeat,int shiftDistance, boolean abs) {
 		for(int rep = 0; rep < repeat; rep++) {
-			int space = pixelArray.length;
+			int space = pixels.length;
 			
 			
 			if(abs) {
 				for(int i = 0; i<space; i++) {
-					swapByteArray(pixelArray[Math.abs((-i + (i*(int)Width)))%space],pixelArray[Math.abs((-i + (i*(int)Width)+shiftDistance))%space]);
+					swapByteArray(pixels[Math.abs((-i + (i*(int)Width)))%space],pixels[Math.abs((-i + (i*(int)Width)+shiftDistance))%space]);
 				}
 				
 				for(int i = 0; i<space; i++) {
-					swapByteArray(pixelArray[Math.abs((i + (i*(int)Width)))%space],pixelArray[Math.abs((i + (i*(int)Width)+shiftDistance))%space]);
+					swapByteArray(pixels[Math.abs((i + (i*(int)Width)))%space],pixels[Math.abs((i + (i*(int)Width)+shiftDistance))%space]);
 				}
 			} else {
 				for(int i = 0; i<space; i++) {
-					swapByteArray(pixelArray[(-i + (i*(int)Width))%space],pixelArray[(-i + (i*(int)Width)+shiftDistance)%space]);
+					swapByteArray(pixels[(-i + (i*(int)Width))%space],pixels[(-i + (i*(int)Width)+shiftDistance)%space]);
 				}
 				
 				for(int i = 0; i<space; i++) {
-					swapByteArray(pixelArray[(i + (i*(int)Width))%space],pixelArray[(i + (i*(int)Width)+shiftDistance)%space]);
+					swapByteArray(pixels[(i + (i*(int)Width))%space],pixels[(i + (i*(int)Width)+shiftDistance)%space]);
 				}
 			}
 		}
@@ -321,11 +335,11 @@ class BitMap {
 	//SHOULD BE NEAT
 	//Not really implemented at all yet
 	void MathDemon(){
-		for(int i = 0; i<pixelArray.length; i++) {
+		for(int i = 0; i<pixels.length; i++) {
 			double x = i%(int)Width;
 			double y = i/(int)Width;
 			if(400*Math.cos(x * Math.pow(.995, (double)x))> y) {
-				pixelArray[i][0] = pixelArray[i][2] = pixelArray[i][1] = (byte)(pixelArray[i][0]&0x00);
+				pixels[i][0] = pixels[i][2] = pixels[i][1] = (byte)(pixels[i][0]&0x00);
 			}
 		}
 	}
@@ -335,12 +349,36 @@ class BitMap {
 		for(int rep = 0; rep < repeat; rep ++) {
 			for(int i = 0, t = 0; i<(int)((FileSize-DataOffset)/(BitCount/8)); i++) {
 				for(int q = 0; q<(int)(BitCount/8); q++,t++) {
-					pixelArray[i][q] = pixelArray[((int)((double)t*slide)%(int)(FileSize-DataOffset))/3][((int)((double)t*slide)%(int)(FileSize-DataOffset))%3];
+					pixels[i][q] = pixels[((int)((double)t*slide)%(int)(FileSize-DataOffset))/3][((int)((double)t*slide)%(int)(FileSize-DataOffset))%3];
 				}
 			}
 		}
 	}
 	
+	void histAdd() {
+		histCount++;
+		pixelsHistory[histCount] = deepCopyPixels(pixels);
+		redos = 0;
+	}
+	
+	void histUndo() {
+		if(histCount > 0) {
+			histCount --;
+			redos ++;
+			pixels = deepCopyPixels(pixelsHistory[histCount]);
+		} else {
+			System.out.println("Could not undo");
+		}
+	}
+	
+	void histRedo() {
+		if(redos > 0) {
+			histCount++;
+			pixels = deepCopyPixels(pixelsHistory[histCount]);
+		} else {
+			System.out.println("Could not redo");
+		}
+	}
 	
 	
 	////
@@ -374,7 +412,7 @@ class BitMap {
 		return out;
 	}
 	
-	void swapByteArray(byte[] ar1, byte[] ar2) {
+	static void swapByteArray(byte[] ar1, byte[] ar2) {
 		byte hold;
 		for(int i = 0; i<ar1.length; i++) {
 			hold = ar1[i];
@@ -383,4 +421,128 @@ class BitMap {
 		}
 	}
 	
+	byte[][] deepCopyPixels(byte[][] source) {
+		byte[][] copy = new byte[source.length][source[0].length];
+		for(int i = 0; i< source.length; i++) {
+			for(int q = 0; q < source[0].length; q++) {
+				copy[i][q] = source[i][q];
+			}
+		}
+		return copy;
+	}
 }
+
+//Takes BitMap as input, and create an array of selected pixels, in the order they would be selected
+//Hold selections in a 
+class Selector {
+	BitMap map;
+	
+	int selectedCount;
+	HashMap<Integer,int[]> selection = new HashMap<Integer,int[]>();
+	
+	Selector(BitMap target) {
+		this.map = target;
+	}
+	
+	int all(int spot){return all(spot,"");}
+	int all(int spot, String options){
+		boolean reverse = false;
+		if(options.contains("-r")) {reverse = true;}
+		
+		selection.put(spot, new int[map.pixels.length]);
+		int[] hold = selection.get(spot);
+		for(int i = 0; i< map.pixels.length; i++) {
+			if(!reverse) {
+				hold[i] = i;
+			} else {
+				hold[i] = map.pixels.length - (i+1);
+			}
+		}
+		return spot;
+	}
+	
+	//TODO
+	int vert(int spot, double start, double end) {
+		selection.put(spot, new int[map.pixels.length]);
+		int[] hold = selection.get(spot);
+		
+		
+		return spot;
+	}
+	
+	//TODO
+	int horz(int spot, double start, double end) {
+		selection.put(spot, new int[map.pixels.length]);
+		int[] hold = selection.get(spot);
+		
+		
+		return spot;
+	}
+}
+
+//Takes a selection as input, and does a per pixel function across the selection
+class Demon {
+	Selector sel;
+	
+	Demon(Selector target) {
+		this.sel = target;
+	}
+	
+	//Overload redirectors
+	void fuzz(int selIndex) {fuzz(new int[]{selIndex},"");}
+	void fuzz(int selIndex, String options) {fuzz(new int[]{selIndex},options);}
+	
+	void fuzz(int[] selIndex, String options) {
+		for(int s : selIndex) {
+			boolean undoable = options.contains("-u");
+			
+			
+			if(undoable) {
+				sel.map.histAdd();
+			}
+			
+			if(sel.selection.containsKey(s)) {
+				int[] hold = sel.selection.get(s);
+				
+				for(int i = 0; i<hold.length; i++) {
+					if(Math.random() > .5) {
+						if(i-1 > 0) {
+							BitMap.swapByteArray(sel.map.pixels[hold[i]],sel.map.pixels[hold[i-1]]);
+						}
+					} else {
+						if(i+1 < sel.map.pixels.length) {
+							BitMap.swapByteArray(sel.map.pixels[hold[i]],sel.map.pixels[hold[i+1]]);
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	//Overload redirectors
+	void swap(int selIndex1, int selIndex2) {swap(new int[]{selIndex1},new int[]{selIndex2}, "");}
+	void swap(int selIndex1, int selIndex2, String options) {swap(new int[]{selIndex1},new int[]{selIndex2}, options);}
+	void swap(int[] selIndex1, int[] selIndex2) {swap(selIndex1,selIndex2, "");}
+	
+	void swap(int[] selIndex1,int[] selIndex2, String options) {
+		for(int s = 0; s<selIndex1.length && s<selIndex2.length; s++) {
+			if(sel.selection.containsKey(selIndex1[s]) && sel.selection.containsKey(selIndex2[s])) {
+				int[] hold1 = sel.selection.get(selIndex1[s]);
+				int[] hold2 = sel.selection.get(selIndex2[s]);
+				
+				for(int i = 0; i<hold1.length && i<hold2.length; i++) {
+					BitMap.swapByteArray(sel.map.pixels[hold1[i]],sel.map.pixels[hold2[i]]);
+				}
+			} else {
+				System.out.println("ERROR, COULD NOT FIND SWAP PAIR (" + selIndex1[s] + "," + selIndex2[s] + ")");
+			}
+		}
+	}
+	
+}
+
+//TODO
+class Loader {
+	
+}
+
